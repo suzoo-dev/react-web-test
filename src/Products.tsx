7eb5ecf6b4ee198b8fb64d5b7a4fa1270f9a5dea
suzoo-dev <57764195+suzoo-dev@users.noreply.github.com>
2024-10-10 01:33:26 +0100

    displayed products

--- src/Products.tsx
+++ src/Products.tsx
@@ -1,18 +1,46 @@
+import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { searchProducts } from "./functions/searchProducts";
-import { useState } from "react";
 
 function Products() {
   const [searchTerm] = useState<string>("");
 
-  const { data } = useQuery({
+  const { data, isLoading, isError } = useQuery({
     queryKey: ["products", searchTerm],
     queryFn: () => searchProducts(searchTerm),
+    retry: false,
   });
 
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Error</p>;
   console.log("data", data);
 
-  return <p>Products</p>;
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Products</h1>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "start",
+          overflow: "auto",
+          textAlign: "left",
+          height: "600px",
+        }}
+      >
+        <ul>
+          {data?.products.map((product) => (
+            <li
+              key={product.id}
+              style={{ borderBottom: "1px solid #ccc", margin: "10px" }}
+            >
+              <h2>{product.title}</h2>
+              <p>{product.description}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Products;
